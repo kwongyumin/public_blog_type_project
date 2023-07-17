4a222100ba17dfd02c3a6a936bd465cd18e6879b
gwongyumin <rbals7398@naver.com>
2023-07-17 16:45:56 +0900

    - 충돌 방지용 fix 브랜치 생성

--- src/main/java/com/example/blog/dto/user/UserRequestDto.java
+++ src/main/java/com/example/blog/dto/user/UserRequestDto.java
@@ -1,8 +1,33 @@
 package com.example.blog.dto.user;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
 public class UserRequestDto {
+
+    /*
+        Note : inner class 관리
+     */
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class JoinUser {
+
+        private String userName;
+
+        private String nickName;
+
+        private String password;
+
+        private String email;
+
+        private String intro;
+
+        private LoginType loginType;
+    }
+
 }
---
+++ src/main/java/com/example/blog/model/user/Profile.java
@@ -0,0 +1,41 @@
+package com.example.blog.model.user;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@EnableJpaAuditing // Auditing 활성화
+@EntityListeners(AuditingEntityListener.class) // Auditing 리스너 등록
+public class Profile {
+
+    /*
+        fixme : merge conflict 유의
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String profileImgUrl;
+
+    @Column
+    private String intro;
+
+    @Column
+    @LastModifiedDate // 수정 시간 자동 업데이트
+    private LocalDateTime modTime;
+
+    @Column(updatable = false)
+    @CreatedDate // 등록 시간 자동 업데이트
+    private LocalDateTime regTime;
+}
